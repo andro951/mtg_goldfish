ae37c7ba1a2e746bbcabe85a38e02c787e26acd6
Isaac Richards <44043352+andro951@users.noreply.github.com>
2026-09-12 04:06:24 -0400

    release: pin the 160-card acceptance allowlist to the tested rules pack

---
+++ src/rules/accepted-cards.js
@@ -0,0 +1,164 @@
+/** Rules-pack 1.0.0 acceptance allowlist. New modules remain implemented, not
+ * accepted, until their card tests and interaction regressions are reviewed.
+ * Full means automated within the documented goldfish scope, not a complete
+ * multiplayer Magic judge. See tests/card-acceptance.test.js and README.md. */
+export const ACCEPTED_CARDS = Object.freeze(`Aesi, Tyrant of Gyre Strait
+Aetherworks Marvel
+Aftermath Analyst
+Ancient Den
+Ancient Greenwarden
+Arcum Dagsson
+Azorius Chancery
+Azusa, Lost but Seeking
+Birthing Pod
+Bolas's Citadel
+Bringer of the White Dawn
+Buried Ruin
+Chromatic Orrery
+Chulane, Teller of Tales
+Clock of Omens
+Codex Shredder
+Combustible Gearhulk
+Crucible of Worlds
+Crystal Skull, Isu Spyglass
+Cultivator Colossus
+Darkmoss Bridge
+Darksteel Citadel
+Dimir Aqueduct
+Displacer Kitten
+Drossforge Bridge
+Eldritch Evolution
+Elven Raft-Steerer
+Elvish Reclaimer
+Empowered Autogenerator
+Emry, Lurker of the Loch
+Esoteric Duplicator
+Expedition Map
+Faith's Reward
+Fateful Discovery
+Field of the Dead
+Fomori Vault
+Gandalf, Shadow's Foe
+Gerrard's Hourglass Pendant
+Goblin Engineer
+Goblin Welder
+Golbez, Crystal Collector
+Goldmire Bridge
+Golgari Rot Farm
+Great Furnace
+Grim Monolith
+Grinding Station
+Gruul Turf
+H.E.R.B.I.E. Scout Unit
+Icetill Explorer
+Inventors' Fair
+Izzet Boilerworks
+Jhoira, Weatherlight Captain
+Kodama of the East Tree
+Krang, Master Mind
+Krang, Utrom Warlord
+Krark-Clan Ironworks
+Kuldotha Forgemaster
+Lightning Greaves
+Llanowar Scout
+Lotus Cobra
+Lumra, Bellow of the Woods
+Mana Vault
+Manabond
+Master Transmuter
+Memory Jar
+Metalwork Colossus
+Metalworker
+Mishra's Workshop
+Mistvault Bridge
+Molderhulk
+Moonsilver Key
+Moraug, Fury of Akoum
+Mox Amber
+Mox Opal
+Myr Battlesphere
+Myr Turbine
+Nahiri's Lithoforming
+Neoform
+Nissa, Resurgent Animist
+Oboro, Palace in the Clouds
+Open the Vaults
+Oswald Fiddlebender
+Panther Robot
+Phantom Train
+Pitiless Carnage
+Portal to Phyrexia
+Power Depot
+Priest of Yawgmoth
+PuPu UFO
+Quicksmith Genius
+Radiant Lotus
+Razortide Bridge
+Redress Fate
+Repurposing Bay
+Retreat to Coralhelm
+Roil Cartographer
+Rook Turret
+Rustvale Bridge
+Rydia, Summoner of Mist
+Sakura-Tribe Scout
+Salvage Titan
+Salvaging Station
+Sarinth Steelseeker
+Scaled Herbalist
+Scapeshift
+Scaretiller
+Scene of the Crime
+Scourglass
+Scrabbling Claws
+Scrap Trawler
+Scroll Rack
+Seat of the Synod
+Second Sunrise
+Seedborn Muse
+Selesnya Sanctuary
+Sensei's Divining Top
+Shimmer Dragon
+Shorikai, Genesis Engine
+Silverbluff Bridge
+Simic Growth Chamber
+Six
+Slagwoods Bridge
+Smelting Vat
+Spine of Ish Sah
+Squandered Resources
+Summon: Bahamut
+Tanglepool Bridge
+Tatyova, Benthic Druid
+Tezzeret the Seeker
+Tezzeret, Cruel Captain
+The Cauldron of Eternity
+The Gitrog Monster
+The Great Henge
+The Mightstone and Weakstone
+The Mycosynth Gardens
+The One Ring
+The Reality Chip
+The Seriema
+The Wandering Minstrel
+Thornglint Bridge
+Thought Monitor
+Tideforce Elemental
+Tireless Provisioner
+Trade Routes
+Transplant Theorist
+Traveling Chocobo
+Treasure Vault
+Tree of Tales
+Ultron, Artificial Malevolence
+Umbral Collar Zealot
+Unwinding Clock
+Urza's Saga
+Urza, Lord High Artificer
+Urza, Lord Protector
+Urza, Prince of Kroog
+Uthros Research Craft
+Valakut Exploration
+Vault of Whispers
+Wake the Past
+Walking Atlas`.split('\n'));
